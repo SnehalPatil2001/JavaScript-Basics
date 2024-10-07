ad79ed398b02f81a2b39c4121c827f50658e4909
Snehal <snehalpatil1282001@gmail.com>
2024-10-07 20:51:38 +0530

    Add functionalities to find Even and Odd numbers ,print pattern and identify positive and negative numbers

---
+++ positiveNegativeNum.js
@@ -0,0 +1,6 @@
+let num=-19;
+if(num>0){
+    console.log("Number is greter than 0 then it is Positive number");
+}else{
+    console.log("Number is smaller than 0 then it is Negative number");  
+}
